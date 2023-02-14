6b4df81c3094cff6890bc238a1d8094fdbe2654d
Antonomy <antonyyu@gmail.com>
2023-02-14 12:46:52 -0800

    Updates

---
+++ precache-manifest.96c3534a8cd5c44820cb2935ac10d924.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d50b0d1939d296022c80",
+    "url": "/static/css/main.f9f526f3.chunk.css"
+  },
+  {
+    "revision": "d50b0d1939d296022c80",
+    "url": "/static/js/main.d50b0d19.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1748fee3edd152fb67b5d433697df4cc",
+    "url": "/index.html"
+  }
+];
