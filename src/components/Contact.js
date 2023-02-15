5ec89f349612f147d6d20b925adf60e08de22eff
Antonomy <antonyyu@gmail.com>
2023-02-14 22:05:42 -0800

    updated socials

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,29 +1,32 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 export default class Contact extends Component {
-  render() {
-    let resumeData = this.props.resumeData;
+  render () {
+    const resumeData = this.props.resumeData
     return (
-      <section id="contact">
-        <div className="row section-head">
-          <div className="ten columns">
-            <p className="lead">
+      <section id='contact'>
+        <div className='row section-head'>
+          <div className='ten columns'>
+            <p className='lead'>
               I am always looking for collaboration opportunities. Feel free to reach out!
             </p>
           </div>
         </div>
-        <div className="row">
-          <aside className="eight columns footer-widgets">
-            <div className="widget">
-              <p  className="lead">
-                <a href="https://github.com/Antonomy" target="_blank"><img alt="Github" src="https://img.shields.io/badge/GitHub-41454A.svg?&style=for-the-badge&logo=Github&logoColor=white" /></a>
-                <a href="https://www.linkedin.com/in/antonyyu/" target="_blank"><img alt="LinkedIn" src="https://img.shields.io/badge/linkedin-%230077B5.svg?&style=for-the-badge&logo=linkedin&logoColor=white" /></a>
-                <a href="mailto:hello@antonyyu.com" target="_blank"><img alt="Email Me" src="https://img.shields.io/badge/Email%20me-EA4335.svg?&style=for-the-badge&logo=gmail&logoColor=white" /></a>
-                <a href="https://calendly.com/antonyyu" target="_blank"><img alt="Book Time on My Calendar" src="https://img.shields.io/badge/Book%20Time%20with%20me-4285F4.svg?&style=for-the-badge&logo=googlecalendar&logoColor=white" /></a>
+        <div className='row'>
+          <aside className='eight columns footer-widgets'>
+            <div className='widget'>
+              <p className='lead'>
+                {
+                resumeData.contactBadge && resumeData.contactBadge.map((badge) => {
+                  return (
+                    <a href={badge.url} target='_blank' rel='noreferrer'><img alt={badge.name} src={badge.img} /></a>
+                  )
+                })
+              }
               </p>
             </div>
           </aside>
         </div>
       </section>
-    );
+    )
   }
 }
--- src/resumeData.js
+++ src/resumeData.js
@@ -4,6 +4,11 @@ const resumeData = {
   landingIntro: "I'm a Full Stack Software Engineer & Program Operations Leader. I'm always looking for opportunities to collaborate on new projects- so feel free to reach out!",
   socialLinks: [
     {
+      name: 'email',
+      url: 'mailto:hello@antonyyu.com',
+      className: 'fa fa-solid fa-envelope'
+    },
+    {
       name: 'linkedin',
       url: 'https://www.linkedin.com/in/antonyyu/',
       className: 'fa fa-linkedin'
@@ -33,8 +38,8 @@ const resumeData = {
       StartMonth: 'Sept',
       StartYear: '2018',
       EndMonth: 'Apr',
-      EndYear: '2020',
-      Achievements: 'PUBG Mobile'
+      EndYear: '2020'
+      // Achievements: 'PUBG Mobile'
     },
     {
       CompanyName: 'Riot Games',
@@ -42,8 +47,8 @@ const resumeData = {
       StartMonth: 'Aug',
       StartYear: '2014',
       EndMonth: 'Sept',
-      EndYear: '2018',
-      Achievements: 'League of Legends'
+      EndYear: '2018'
+      // Achievements: 'League of Legends'
     }
   ],
   education: [
@@ -92,6 +97,28 @@ const resumeData = {
     },
     {
       skillname: 'Node'
+    }
+  ],
+  contactBadge: [
+    {
+      url: 'https://github.com/Antonomy',
+      name: 'GitHub',
+      img: 'https://img.shields.io/badge/GitHub-41454A.svg?&style=for-the-badge&logo=Github&logoColor=white'
+    },
+    {
+      url: 'https://www.linkedin.com/in/antonyyu/',
+      name: 'LinkedIn',
+      img: 'https://img.shields.io/badge/linkedin-%230077B5.svg?&style=for-the-badge&logo=linkedin&logoColor=white'
+    },
+    {
+      url: 'mailto:hello@antonyyu.com',
+      name: 'Email Me',
+      img: 'https://img.shields.io/badge/Email%20me-EA4335.svg?&style=for-the-badge&logo=gmail&logoColor=white'
+    },
+    {
+      url: 'https://calendly.com/antonyyu',
+      name: 'Book Time on My Calendar',
+      img: 'https://img.shields.io/badge/Book%20Time%20with%20me-4285F4.svg?&style=for-the-badge&logo=googlecalendar&logoColor=white'
     }
   ]
 }
